6b6f743c0d908e96342439f5b6e87562e5f38ca0
Linda Zou <49109592+lindafz@users.noreply.github.com>
2021-06-11 15:15:02 -0700

    Initial check in

---
+++ src/main/java/com/lindazf/primeness/PrimenessApplication.java
@@ -0,0 +1,13 @@
+package com.lindazf.primeness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimenessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimenessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lindazf/primeness/PrimenessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lindazf.primeness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimenessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
